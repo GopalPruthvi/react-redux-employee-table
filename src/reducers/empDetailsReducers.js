493486d10325b52c3546ea55cabfb4e7dc7fa192
GopalPruthvi <91323863+GopalPruthvi@users.noreply.github.com>
2022-04-12 15:31:30 +0530

    Add files via upload

---
+++ src/reducers/empDetailsReducers.js
@@ -0,0 +1,41 @@
+const initialData = {
+    employees: [
+        {
+            id: 1,
+            name: "Pruthvi",
+            empid: 100,
+            designation: "Software Engineer",
+            manager: "Abhimanyu",
+            exp: 3,
+            address: "Chikkballapur",
+            is_Manager: false,
+          }
+    ]
+}
+
+const empDetailsReducers =(state= initialData, action)=>{
+    switch (action.type){
+        case "ADD_DETAILS":
+          const {id, name} = action.payload;
+           return{
+               ...state,
+               employees:[
+                   ...state.employees,
+                {
+                    id: id,
+                    name:name.name,
+                    empid: name.empid,
+                    designation:name.designation,
+                    manager:name.manager,
+                    exp: name.exp,
+                    address: name.address,
+                    is_Manager: name.is_Manager
+               }]
+           }
+        
+        default : return state
+    }
+
+}
+
+export default empDetailsReducers;
